fabf4ae4b23a23d4202d2115db88d512ec15f39d
Pedro Pablo Osorio San Martin <pedro.osorio04@inacapmail.com>
2025-10-09 16:04:22 -0300

    Se hacen pruebas para ver si tailwind css funciona

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,6 +5,11 @@ export default function Home() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">MAQTEC ERP</h1>
+      <div className="flex flex-col items-center justify-center">
+        <p className="text-lg text-gray-700">
+          Bienvenido a MAQTEC ERP, tu solución integral para la gestión empresarial.
+        </p>
+      </div>
     </div>
   );
 }
